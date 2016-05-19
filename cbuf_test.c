134552d01dc4bed33e0a922eb5784bce0917a435
Sirio Balmelli <sirio.bm@gmail.com>
2016-05-19 19:49:36 -0400

    Tweak test code
    Remove redundant code by implementing a 'use_malloc' variable for
    	all the test functions.
    
    Have test_cbuf_threaded() reset global variables, else the
    	second iteration of threaded testing will inevitably fail
    	with wrong global counts.

--- cbuf_test.c
+++ cbuf_test.c
@@ -18,12 +18,9 @@ struct sequence {
 	char		padding[48];
 };
 
-int test_cbuf_single();
-int test_cbuf_single_malloc();
-int test_cbuf_steps();
-int test_cbuf_steps_malloc();
-int test_cbuf_threaded();
-int test_cbuf_threaded_malloc();
+int test_cbuf_single(int use_malloc);
+int test_cbuf_steps(int use_malloc);
+int test_cbuf_threaded(int use_malloc);
 void *snd_thread(void *args);
 void *rcv_thread(void *args);
 
@@ -52,37 +49,37 @@ int main()
 
 	start = clock();
 	Z_inf(0, "single thread, single step");
-	err_cnt += test_cbuf_single();
+	err_cnt += test_cbuf_single(0);
 	start = clock() - start;
 	Z_inf(0, "ELAPSED: %ld", start);
        
   	start = clock();
-	Z_inf(0, "single thread, single step malloc");
-	err_cnt += test_cbuf_single_malloc();
+	Z_inf(0, "single thread, single step (malloc'ed)");
+	err_cnt += test_cbuf_single(1);
 	start = clock() - start;
 	Z_inf(0, "ELAPSED: %ld", start);
 
 	start = clock();
 	Z_inf(0, "single thread, stepped: %d", STEP_SIZE);
-	err_cnt += test_cbuf_steps();
+	err_cnt += test_cbuf_steps(0);
 	start = clock() - start;
 	Z_inf(0, "ELAPSED: %ld", start);
 
     	start = clock();
-	Z_inf(0, "single thread, stepped malloc: %d", STEP_SIZE);
-	err_cnt += test_cbuf_steps_malloc();
+	Z_inf(0, "single thread, stepped (malloc'ed): %d", STEP_SIZE);
+	err_cnt += test_cbuf_steps(1);
 	start = clock() - start;
 	Z_inf(0, "ELAPSED: %ld", start);
 
-/*  	start = clock();
-	Z_inf(0, "%d threads, stepped: %d", THREAD_CNT, STEP_SIZE);
-	err_cnt += test_cbuf_threaded();
-	start = clock() - start;
-	Z_inf(0, "ELAPSED: %ld", start);
-*/
   	start = clock();
 	Z_inf(0, "%d threads, stepped: %d", THREAD_CNT, STEP_SIZE);
-	err_cnt += test_cbuf_threaded_malloc();
+	err_cnt += test_cbuf_threaded(0);
+	start = clock() - start;
+	Z_inf(0, "ELAPSED: %ld", start);
+
+  	start = clock();
+	Z_inf(0, "%d threads, stepped (malloc'ed): %d", THREAD_CNT, STEP_SIZE);
+	err_cnt += test_cbuf_threaded(1);
 	start = clock() - start;
 	Z_inf(0, "ELAPSED: %ld", start);
 
@@ -95,10 +92,14 @@ Tests a single-sender -> single-receiver configuration.
 
 Re: turns 0 on success.
 	*/
-int test_cbuf_single()
+int test_cbuf_single(int use_malloc)
 {
 	int err_cnt = 0;
-	cbuf_t *c = cbuf_create(OBJ_SZ, OBJ_CNT);
+	cbuf_t *c = NULL;
+	if (use_malloc)
+		c = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
+	else
+		c = cbuf_create(OBJ_SZ, OBJ_CNT);
 	Z_die_if(!c, "expecting buffer");
 
 	int i;
@@ -125,118 +126,55 @@ out:
 	return err_cnt;
 }
 
-/*	test_cbuf_single_malloc()
-Tests a single-sender -> single-receiver configuration.
-Malloc'ed case.
-Returns 0 on success.
+int test_cbuf_steps(int use_malloc)
+{
+	int err_cnt = 0;
+	cbuf_t *c = NULL;
+	if (use_malloc)
+		c = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
+	else
+		c = cbuf_create(OBJ_SZ, OBJ_CNT);
+	Z_die_if(!c, "expecting buffer");
+
+	int i, j;
+	uint32_t pos;
+	struct sequence *seq;
+	/* mult. by thread count so we do the same total iters as the threaded test */
+	for (i=0; i < NUMITER * THREAD_CNT; i += STEP_SIZE) {
+		/* reserve a 'step' of write blocks */
+		pos = cbuf_snd_res_m(c, STEP_SIZE);
+		Z_die_if(pos == -1, "send reservation");
+		/* write all blocks */
+		for (j=0; j < STEP_SIZE; j++) {
+			seq = cbuf_offt(c, pos, j); 
+			seq->i = i+j;
+		}
+		/* release */
+		cbuf_snd_rls_m(c, STEP_SIZE);
+
+		/* do the same for the receive side */
+		pos = cbuf_rcv_res_m(c, STEP_SIZE);
+		Z_die_if(pos == -1, "receive reservation");
+		for (j=0; j < STEP_SIZE; j++) {
+			seq = cbuf_offt(c, pos, j);
+			Z_die_if(seq->i != i+j, "wrong data");
+		}
+		cbuf_rcv_rls_m(c, STEP_SIZE);
+	}
+
+out:
+	if (c)
+		cbuf_free(c);
+	return err_cnt;
+}
+
+/*	test_cbuf_threaded()
+Creates a cbuf, then runs a multithreaded IO test on it.
+If 'use_malloc' is set, uses a cbuf_malloc().
+
+returns 0 on success.
 	*/
-int test_cbuf_single_malloc()
-{
-	int err_cnt = 0;
-	cbuf_t *c = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
-	Z_die_if(!c, "expecting malloc'ed buffer");
-
-	int i;
-	struct sequence *seq;
-	/* mult. by thread count so we do the same total iters as the threaded test */
-	for (i=0; i < NUMITER * THREAD_CNT; i++) {
-		/* reserve a 'step' of write blocks */
-		seq = cbuf_snd_res(c);
-		Z_die_if(!seq, "sender single reservation");
-		seq->i = i;
-		/* release */
-		cbuf_snd_rls(c);
-
-		/* do the same for the receive side */
-		seq = cbuf_rcv_res(c);
-		Z_die_if(!seq, "receive reservation");
-		Z_die_if(seq->i != i, "wrong data");
-		cbuf_rcv_rls(c);
-	}
-
-out:
-	if (c)
-		cbuf_free(c);
-	return err_cnt;
-}
-
-int test_cbuf_steps()
-{
-	int err_cnt = 0;
-	cbuf_t *c = cbuf_create(OBJ_SZ, OBJ_CNT);
-	Z_die_if(!c, "expecting buffer");
-
-	int i, j;
-	uint32_t pos;
-	struct sequence *seq;
-	/* mult. by thread count so we do the same total iters as the threaded test */
-	for (i=0; i < NUMITER * THREAD_CNT; i += STEP_SIZE) {
-		/* reserve a 'step' of write blocks */
-		pos = cbuf_snd_res_m(c, STEP_SIZE);
-		Z_die_if(pos == -1, "send reservation");
-		/* write all blocks */
-		for (j=0; j < STEP_SIZE; j++) {
-			seq = cbuf_offt(c, pos, j); 
-			seq->i = i+j;
-		}
-		/* release */
-		cbuf_snd_rls_m(c, STEP_SIZE);
-
-		/* do the same for the receive side */
-		pos = cbuf_rcv_res_m(c, STEP_SIZE);
-		Z_die_if(pos == -1, "receive reservation");
-		for (j=0; j < STEP_SIZE; j++) {
-			seq = cbuf_offt(c, pos, j);
-			Z_die_if(seq->i != i+j, "wrong data");
-		}
-		cbuf_rcv_rls_m(c, STEP_SIZE);
-	}
-
-out:
-	if (c)
-		cbuf_free(c);
-	return err_cnt;
-}
-
-int test_cbuf_steps_malloc()
-{
-	int err_cnt = 0;
-	cbuf_t *c = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
-	Z_die_if(!c, "expecting malloc'ed buffer");
-
-	int i, j;
-	uint32_t pos;
-	struct sequence *seq;
-	/* mult. by thread count so we do the same total iters as the threaded test */
-	for (i=0; i < NUMITER * THREAD_CNT; i += STEP_SIZE) {
-		/* reserve a 'step' of write blocks */
-		pos = cbuf_snd_res_m(c, STEP_SIZE);
-		Z_die_if(pos == -1, "send reservation");
-		/* write all blocks */
-		for (j=0; j < STEP_SIZE; j++) {
-			seq = cbuf_offt(c, pos, j); 
-			seq->i = i+j;
-		}
-		/* release */
-		cbuf_snd_rls_m(c, STEP_SIZE);
-
-		/* do the same for the receive side */
-		pos = cbuf_rcv_res_m(c, STEP_SIZE);
-		Z_die_if(pos == -1, "receive reservation");
-		for (j=0; j < STEP_SIZE; j++) {
-			seq = cbuf_offt(c, pos, j);
-			Z_die_if(seq->i != i+j, "wrong data");
-		}
-		cbuf_rcv_rls_m(c, STEP_SIZE);
-	}
-
-out:
-	if (c)
-		cbuf_free(c);
-	return err_cnt;
-}
-
-int test_cbuf_threaded()
+int test_cbuf_threaded(int use_malloc)
 {
 	int err_cnt = 0;
 	
@@ -253,81 +191,18 @@ int test_cbuf_threaded()
 			@8 : 0+1+2+3+4+5+6+7 = (8-1)*0.5*8 = 28
 	*/
 	uint64_t final_verif = (NUMITER -1) * 0.5 * NUMITER * THREAD_CNT;
+	/* reset global sums
+	(otherwise test will fail when run more than once)
+		*/
+	global_sum = expected_sum = 0;
 
 	/* circ buf */
-	cbuf_t *buf = cbuf_create(OBJ_SZ, OBJ_CNT);
+	cbuf_t *buf = NULL;
+	if (use_malloc)
+		buf = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
+	else 
+		buf = cbuf_create(OBJ_SZ, OBJ_CNT);
 	Z_die_if(!buf, "fail to alloc");
-
-	/* launch senders & receivers */
-	int i;
-	pthread_t snd_thr[THREAD_CNT];
-	pthread_t rcv_thr[THREAD_CNT];
-	for (i=0; i < THREAD_CNT; i++) {
-		rcv_thr[i] = mts_launch(rcv_thread, buf, mts_free_noop, NULL);
-		snd_thr[i] = mts_launch(snd_thread, buf, mts_free_noop, NULL);
-	}
-
-	/* recover sender threads, and get amount of busywaits */
-	void *ret;
-	uint64_t busywait_snd = 0;
-	for (i=0; i < THREAD_CNT; i++) {
-		pthread_join(snd_thr[i], &ret);
-		busywait_snd += (uint64_t)ret;
-	}
-	Z_inf(0, "senders: %ld waits", busywait_snd);
-
-	/* receiver threads */
-	uint64_t busywait_rcv = 0;
-	for (i=0; i < THREAD_CNT; i++) {
-		pthread_join(rcv_thr[i], &ret);
-		busywait_rcv += (uint64_t)ret;
-	}
-	Z_inf(0, "receivers: %ld waits", busywait_rcv);
-
-	/* verify non-overlap of cbuf reservations */
-	uint32_t exp_pos = (cbuf_sz_obj(buf) * NUMITER * THREAD_CNT) & buf->overflow_;
-	Z_err_if(exp_pos != (buf->snd_pos & buf->overflow_), 
-		"exp_pos %d != snd_pos %d\n \
-		obj_sz %d, NUMITER %d, THREAD_CNT %d, overflow 0x%x", 
-		exp_pos, buf->snd_pos, cbuf_sz_obj(buf), 
-		NUMITER, THREAD_CNT, buf->overflow_);
-	/* verify that expected sum is correct */
-	Z_err_if(expected_sum != final_verif, 
-		"expected_sum %ld - final_verif %ld = %ld",
-		expected_sum, final_verif, expected_sum - final_verif);
-	/* verify integrity of data */
-	Z_err_if(global_sum != expected_sum,
-		"global_sum %ld - expected_sum %ld = %ld\n\
-		... this is known to fail from time to time and Sirio has no clue WHY ;(((",
-		global_sum, expected_sum, global_sum - expected_sum);
-
-out:
-	if (buf)
-		cbuf_free(buf);
-	return err_cnt;
-}
-
-int test_cbuf_threaded_malloc()
-{
-	int err_cnt = 0;
-	
-	/* this is the expected sum of all 'i' loop iterators for all threads 
-		The logic to arrive at this was:
-			@1 : i = 0		0/1 = 0
-			@2 : i = 0+1		2/1 = 0.5
-			@3 : i = 0+1+2		3/3 = 1
-			@4 : i = 0+1+2+3	6/4 = 1.5
-			@5 : i = 0+1+2+3+4	10/5 = 2
-			...
-			@8 : i = 0+1+2+3+4+5+6+7	28/8	= 3.5
-			@8 : (8-1)*0.5				= 3.5
-			@8 : 0+1+2+3+4+5+6+7 = (8-1)*0.5*8 = 28
-	*/
-	uint64_t final_verif = (NUMITER -1) * 0.5 * NUMITER * THREAD_CNT;
-
-	/* circ buf */
-	cbuf_t *buf = cbuf_create_malloc(OBJ_SZ, OBJ_CNT);
-	Z_die_if(!buf, "fail to alloc malloc");
 
 	/* launch senders & receivers */
 	int i;
